afe0fe12cc3799bf10703566f51ecd52a740d087
trainerpemrograman <trainer_pemrograman@gamelab.id>
2023-12-07 17:03:29 +0700

    Menggabungkan fitur ke main

--- kalkulator.py
+++ kalkulator.py
@@ -6,3 +6,13 @@ def kurang(a, b):
 
 def pangkat(a, b):
     return a ** b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Pembagian oleh nol"
+

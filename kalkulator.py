530698400ec60b05d683f3895d2997000dfc95ef
trainerpemrograman <trainer_pemrograman@gamelab.id>
2023-12-07 16:53:44 +0700

    Cherry-pick commit tertentu dari fitur ke main

--- kalkulator.py
+++ kalkulator.py
@@ -4,8 +4,6 @@ def tambah(a, b):
 def kurang(a, b):
     return a - b
 
-def pangkat(a, b):
-    return a ** b
 
 def kali(a, b):
     return a * b
@@ -15,4 +13,3 @@ def bagi(a, b):
         return a / b
     else:
         return "Error: Pembagian oleh nol"
-
